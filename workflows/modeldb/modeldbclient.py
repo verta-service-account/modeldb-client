659d3c14a7b0d7175cc76aa01e3af7ba7fb337bf
Michael Liu <miliu@protonmail.com>
2019-02-01 12:05:59 -0800

    Define Proj, Expt classes

--- workflows/modeldb/modeldbclient.py
+++ workflows/modeldb/modeldbclient.py
@@ -1,4 +1,7 @@
+from typing import Any, Union, Tuple
+
 import os, sys
+import time
 
 import grpc
 
@@ -16,11 +19,8 @@ class ModelDBClient:
     def __init__(self, host=DEFAULT_HOST, port=DEFAULT_PORT):
         self.channel = grpc.insecure_channel(host + ":" + port)
 
-        self.proj_stub = ProjectService_pb2_grpc.ProjectServiceStub(self.channel)
-        self.expt_stub = ExperimentService_pb2_grpc.ExperimentServiceStub(self.channel)
-
-        self.proj_id = None
-        self.expt_id = None
+        self.proj = None
+        self.expt = None
         self.expt_runs = []
 
     def __enter__(self):
@@ -34,40 +34,117 @@ class ModelDBClient:
         self.channel.close()
         self.channel = None
 
-        self.proj_stub = None
-        self.expt_stub = None
-
-        self.proj_id = None
-        self.expt_id = None
+        self.proj = None
+        self.expt = None
         self.expt_runs = []
 
-    def start_project(self, proj_name):
+    def set_project(self, proj_name=None):
         # TODO: handle case when project is already in progress
-        msg = ProjectService_pb2.CreateProject(name=proj_name)
-        response = self.proj_stub.createProject(msg)  # TODO: verify response
 
-        self.proj_id = response.project.id
-        return self.proj_id
+        proj = Project(self.channel, proj_name)
 
-    def start_experiment(self, expt_name):
+        self.proj = proj
+        return proj
+
+    def set_experiment(self, expt_name=None):
         # TODO: handle case when experiment is already in progress
-        if self.proj_id is None:
+        if self.proj is None:
             raise AttributeError("a project must first in progress")
-        msg = ExperimentService_pb2.CreateExperiment(project_id=self.proj_id,
-                                                     name=expt_name)
-        response = self.expt_stub.createExperiment(msg)  # TODO: verify response
 
-        self.expt_id = response.experiment.id
-        return self.expt_id
+        expt = Experiment(self.channel, self.proj.id, expt_name)
 
-    def create_experiment_run(self, expt_run_name):
-        if self.proj_id is None:
+        self.expt = expt
+        return expt
+
+    def set_experiment_run(self, expt_run_name=None):
+        if self.proj is None:
             raise AttributeError("a project must first in progress")
-        if self.expt_id is None:
+        if self.expt is None:
             raise AttributeError("an experiment must first in progress")
+
         expt_run = ExperimentRun(self.channel,
-                                 self.proj_id, self.expt_id,
+                                 self.proj.id, self.expt.id,
                                  expt_run_name)
 
         self.expt_runs.append(expt_run)
         return expt_run
+
+    def set_experiment_runs(self):
+        if self.proj is None:
+            raise AttributeError("a project must first in progress")
+
+        stub = ExperimentRunService_pb2_grpc.ExperimentRunServiceStub(self.channel)
+        msg = ExperimentRunService_pb2.GetExperimentRunsInProject(project_id=self.proj.id)
+        response = stub.getExperimentRunsInProject(msg)  # TODO: verify response
+
+        self.expt_runs = [ExperimentRun(self.channel,
+                                        self.proj.id, self.expt.id,
+                                        expt_run.name)
+                          for expt_run in response.experiment_runs]
+
+
+class Project:
+    def __init__(self, channel, proj_name=None):
+        if proj_name is None:
+            proj_name = Project.generate_default_name()
+
+        stub = ProjectService_pb2_grpc.ProjectServiceStub(channel)
+
+        try:
+            proj = Project.get(channel, proj_name)
+        except grpc.RpcError:  # when no Projects in Mongo
+            proj = None
+        if proj is None:
+            msg = ProjectService_pb2.CreateProject(name=proj_name)
+            response = stub.createProject(msg)  # TODO: verify response
+            proj = response.project
+
+        self.stub = stub
+        self.id = proj.id
+
+    @staticmethod
+    def get(channel, proj_name):
+        stub = ProjectService_pb2_grpc.ProjectServiceStub(channel)
+        msg = ProjectService_pb2.GetProjects()
+        response = stub.getProjects(msg)  # TODO: verify response
+
+        result = [proj for proj in response.projects if proj.name == proj_name]
+        return result[-1] if len(result) else None
+
+    @staticmethod
+    def generate_default_name():
+        return "Project {}".format(int(time.time()))
+
+
+class Experiment:
+    def __init__(self, channel, proj_id, expt_name=None):
+        if expt_name is None:
+            expt_name = Experiment.generate_default_name()
+
+        stub = ExperimentService_pb2_grpc.ExperimentServiceStub(channel)
+
+        try:
+            expt = Experiment.get(channel, proj_id, expt_name)
+        except grpc.RpcError:  # when Project has no Experiments
+            expt = None
+        if expt is None:
+            msg = ExperimentService_pb2.CreateExperiment(project_id=proj_id,
+                                                         name=expt_name)
+            response = stub.createExperiment(msg)  # TODO: verify response
+            expt = response.experiment
+
+        self.stub = stub
+        self.id = expt.id
+
+    @staticmethod
+    def get(channel, proj_id, expt_name):
+        stub = ExperimentService_pb2_grpc.ExperimentServiceStub(channel)
+        msg = ExperimentService_pb2.GetExperimentsInProject(project_id=proj_id)
+        response = stub.getExperimentsInProject(msg)  # TODO: verify response
+
+        result = [expt for expt in response.experiments if expt.name == expt_name]
+        return result[-1] if len(result) else None
+
+    @staticmethod
+    def generate_default_name():
+        return "Experiment {}".format(int(time.time()))
