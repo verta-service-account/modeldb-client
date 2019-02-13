e26b4b1c6c2b9f05d271732a3a24d771b5058229
Michael Liu <miliu@protonmail.com>
2019-02-13 14:15:35 -0800

    Create scripts for data generation

---
+++ workflows/datagen/income.py
@@ -0,0 +1,89 @@
+import os
+import itertools
+import time
+
+import joblib
+
+import numpy as np
+
+from sklearn import model_selection
+from sklearn import linear_model
+from sklearn import metrics
+
+from verta import ModelDBClient
+
+
+# set these
+EMAIL =
+DEV_KEY =
+# change this, if you'd like
+GRID = {'penalty': ['l1', 'l2'],
+        'C': [1e-4, 1e-3, 1e-2, 1e-1]}
+
+
+# no need to touch anything else
+PROJECT_NAME = "Income Classification"
+EXPERIMENT_NAME = "Logistic Regression"
+
+client = ModelDBClient(EMAIL, DEV_KEY)
+proj = client.set_project(PROJECT_NAME)
+expt = client.set_experiment(EXPERIMENT_NAME)
+
+TRAIN_DATA_PATH = os.path.join("..", "data", "census", "train.npz")
+TEST_DATA_PATH = os.path.join("..", "data", "census", "test.npz")
+MODEL_PATH = os.path.join("..", "output", "client-demo", "logreg_gridsearch_{}.gz")
+
+train_data = np.load(TRAIN_DATA_PATH)
+X_train, y_train = train_data['X'], train_data['y']
+
+grid = [dict(zip(GRID.keys(), values))
+        for values
+        in itertools.product(*GRID.values())]
+
+for hyperparams in grid:
+    start_time = int(time.time())
+
+    # create object to track experiment run
+    run = client.set_experiment_run()
+
+    # log data
+    run.log_dataset("train_data", TRAIN_DATA_PATH)
+    run.log_dataset("test_data", TEST_DATA_PATH)
+
+    # create validation split
+    X_train, X_val, y_train, y_val = model_selection.train_test_split(X_train, y_train,
+                                                                      test_size=0.1, shuffle=False)
+    # log hyperparameters
+    for key, val in hyperparams.items():
+        run.log_hyperparameter(key, val)
+    print(hyperparams, end=' ')
+
+    # create and train model
+    model = linear_model.LogisticRegression(solver='liblinear', **hyperparams)
+    model.fit(X_train, y_train)
+
+    # calculate and log validation accuracy
+    val_acc = model.score(X_val, y_val)
+    run.log_metric("val_acc", val_acc)
+    print("Validation accuracy: {:.4f}".format(val_acc))
+
+    # log model
+    run.log_model("model", MODEL_PATH.format(start_time))
+
+best_run = sorted(client.expt_runs, key=lambda run: run.get_metrics()['val_acc'])[-1]
+
+best_hyperparams = best_run.get_hyperparameters()
+best_val_acc = best_run.get_metrics()['val_acc']
+print("{} Validation accuracy: {:.4f}".format(best_hyperparams, best_val_acc))
+
+model = linear_model.LogisticRegression(**best_hyperparams)
+model.fit(X_train, y_train)
+train_acc = model.score(X_train, y_train)
+best_run.log_metric("train_acc", train_acc)
+print("Training accuracy: {:.4f}".format(train_acc))
+
+test_data = np.load(TEST_DATA_PATH)
+X_test, y_test = test_data['X'], test_data['y']
+test_acc = model.score(X_test, y_test)
+best_run.log_metric("test_acc", test_acc)
+print("Testing accuracy: {:.4f}".format(test_acc))
---
+++ workflows/datagen/mnist.py
@@ -0,0 +1,91 @@
+import os
+import itertools
+import time
+
+import numpy as np
+
+import tensorflow as tf
+from tensorflow import keras
+
+import matplotlib.pyplot as plt
+
+from verta import ModelDBClient
+
+# set these
+EMAIL =
+DEV_KEY =
+# change this, if you'd like
+GRID = {'hidden_size': [512, 1024],
+        'dropout': [.2, .4],
+        'batch_size': [512]}
+
+
+# no need to touch anything else
+PROJECT_NAME = "MNIST Multiclassification"
+EXPERIMENT_NAME = "FC-NN"
+
+client = ModelDBClient(EMAIL, DEV_KEY)
+proj = client.set_project(PROJECT_NAME)
+expt = client.set_experiment(EXPERIMENT_NAME)
+
+TRAIN_DATA_PATH = os.path.join("..", "data", "mnist", "train.npz")
+TEST_DATA_PATH = os.path.join("..", "data", "mnist", "test.npz")
+VAL_PLOT_PATH = os.path.join("..", "output", "val_obs_{}.png")
+MODEL_PATH = os.path.join("..", "output", "tensorflow-basic_{}.hdf5")
+
+OPTIMIZER = 'adam'
+LOSS = 'sparse_categorical_crossentropy'
+NUM_EPOCHS = 4
+VALIDATION_SPLIT = 0.1
+
+train_data = np.load(TRAIN_DATA_PATH)
+X_train, y_train = train_data['X'], train_data['y']
+
+grid = [dict(zip(GRID.keys(), values))
+        for values
+        in itertools.product(*GRID.values())]
+
+for hyperparams in grid:
+    start_time = int(time.time())
+
+    run = client.set_experiment_run()
+    print(hyperparams)
+
+    run.log_dataset("train_data", TRAIN_DATA_PATH)
+    run.log_dataset("test_data", TEST_DATA_PATH)
+
+    model = keras.models.Sequential()
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(hyperparams['hidden_size'], activation=tf.nn.relu))
+    model.add(keras.layers.Dropout(hyperparams['dropout']))
+    model.add(keras.layers.Dense(10, activation=tf.nn.softmax))
+    run.log_hyperparameter("hidden_size", hyperparams['hidden_size'])
+    run.log_hyperparameter("dropout", hyperparams['dropout'])
+
+    model.compile(optimizer=OPTIMIZER,
+              loss=LOSS,
+              metrics=['accuracy'])
+    run.log_hyperparameter("optimizer", OPTIMIZER)
+    run.log_hyperparameter("loss", LOSS)
+
+    def log_validation(epoch, logs):  # Keras will call this each epoch
+        run.log_observation("val_train loss", float(logs['loss']))
+        run.log_observation("val_train acc", float(logs['acc']))
+        run.log_observation("val_loss", float(logs['val_loss']))
+        run.log_observation("val_acc", float(logs['val_acc']))
+    _ = model.fit(X_train, y_train, validation_split=VALIDATION_SPLIT,
+                  batch_size=hyperparams['batch_size'], epochs=NUM_EPOCHS,
+                  callbacks=[keras.callbacks.LambdaCallback(on_epoch_end=log_validation)])
+    run.log_hyperparameter("batch_size", hyperparams['batch_size'])
+    run.log_hyperparameter("num_epochs", NUM_EPOCHS)
+    run.log_hyperparameter("validation_split", VALIDATION_SPLIT)
+
+    plt.plot(run.get_observations("val_acc"), label="val")
+    plt.plot(run.get_observations("val_train_acc"), label="train")
+    plt.ylim(0, 1)
+    plt.xlabel("epoch")
+    plt.ylabel("accuracy")
+    plt.legend(loc='best')
+    run.log_image("validation_plot", VAL_PLOT_PATH.format(start_time))
+
+    run.log_model("model", MODEL_PATH.format(start_time))
