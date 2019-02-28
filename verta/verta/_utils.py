282ae21fb1ff0daa7e0cef90747e3e73a92b3695
Michael Liu <miliu@protonmail.com>
2019-02-27 16:32:14 -0800

    Reimplement conversion utils

--- verta/verta/_utils.py
+++ verta/verta/_utils.py
@@ -1,8 +1,7 @@
 import json
 
 from google.protobuf import json_format
-
-from ._protos.public.modeldb import CommonService_pb2 as _CommonService
+from google.protobuf.struct_pb2 import Value, NULL_VALUE
 
 
 def jsonify(msg):
@@ -11,18 +10,39 @@ def jsonify(msg):
                                                 use_integers_for_enums=True))
 
 
-def get_proto_type(val):
-    if isinstance(val, float) or isinstance(val, int):
-        return _CommonService.ValueTypeEnum.NUMBER
+def to_msg(val):
+    if val is None:
+        return Value(null_value=NULL_VALUE)
+    if isinstance(val, bool):  # did you know that `bool` is a subclass of `int`?
+        return Value(bool_value=val)
+    elif isinstance(val, float) or isinstance(val, int):
+        return Value(number_value=val)
+    elif isinstance(val, str):
+        return Value(string_value=val)
+    elif isinstance(val, dict):
+        raise NotImplementedError()
+    elif isinstance(val, list):
+        raise NotImplementedError()
     else:
-        return _CommonService.ValueTypeEnum.STRING
+        raise ValueError("unsupported type {}".format(type(val)))
 
 
-def cast_to_python(val, proto_type):
-    if proto_type == "NUMBER":
-        try:
-            return int(val)
-        except ValueError:
-            return float(val)
+def to_python(val):
+    if val.HasField("null_value"):
+        return None
+    if val.HasField("bool_value"):
+        return val.bool_value
+    if val.HasField("number_value"):
+        number_value = val.number_value
+        if number_value.is_integer():
+            return int(number_value)
+        else:
+            return number_value
+    if val.HasField("string_value"):
+        return val.string_value
+    if val.HasField("struct_value"):
+        raise NotImplementedError()
+    if val.HasField("list_value"):
+        raise NotImplementedError()
     else:
-        return str(val)
+        raise ValueError("Value is empty")
