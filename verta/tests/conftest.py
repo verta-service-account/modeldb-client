37872641ded308c00ab433ad8d545a3794177a4d
Michael Liu <miliu@protonmail.com>
2019-03-20 11:51:03 -0700

    Create client fixture

---
+++ verta/tests/conftest.py
@@ -0,0 +1,41 @@
+import os
+import pytest
+
+from verta import ModelDBClient
+
+
+ENV_VARS = {
+    'host': "MODELDB_HOST",
+    'port': "MODELDB_PORT",
+    'email': "MODELDB_EMAIL",
+    'dev_key': "MODELDB_DEV_KEY",
+}
+DEFAULTS = {
+    'host': "localhost",
+    'port': "8080",
+    'email': None,
+    'dev_key': None,
+}
+
+
+@pytest.fixture
+def client():
+    kwargs = DEFAULTS.copy()
+    for key in kwargs.keys():
+        try:
+            kwargs[key] = os.environ[ENV_VARS[key]]
+        except KeyError:
+            print("${} not found; using default value {}".format(ENV_VARS[key], kwargs[key]))
+        else:
+            print("${} found; using value {}".format(ENV_VARS[key], kwargs[key]))
+
+    client = ModelDBClient(**kwargs)
+
+    return client
+
+
+@pytest.fixture
+def run(client):
+    client.set_project()
+    client.set_experiment()
+    return client.set_experiment_run()
