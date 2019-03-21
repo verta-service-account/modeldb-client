407117fefb768152d537b355a0650f233122fa3e
Michael Liu <miliu@protonmail.com>
2019-03-21 11:08:08 -0700

    Create delete_* utils

--- verta/tests/utils.py
+++ verta/tests/utils.py
@@ -18,3 +18,33 @@ def gen_float(start=1, stop=None):
         return random.random()*start
     else:
         return random.uniform(start, stop)
+
+
+def delete_project(id_, client):
+    response = requests.get("http://{}/v1/experiment/getExperimentsInProject".format(client._socket),
+                            params={'project_id': id_}, headers=client._auth)
+    response.raise_for_status()
+    for experiment in response.json().get('experiments', []):
+        delete_experiment(experiment['id'], client)
+
+    response = requests.post("http://{}/v1/project/deleteProject".format(client._socket),
+                             json={'id': id_}, headers=client._auth)
+    response.raise_for_status()
+
+
+def delete_experiment(id_, client):
+    response = requests.get("http://{}/v1/experiment-run/getExperimentRunsInExperiment".format(client._socket),
+                            params={'experiment_id': id_}, headers=client._auth)
+    response.raise_for_status()
+    for experiment_run in response.json().get('experiment_runs', []):
+        delete_experiment_run(experiment_run['id'], client)
+
+    response = requests.post("http://{}/v1/experiment/deleteExperiment".format(client._socket),
+                             json={'id': id_}, headers=client._auth)
+    response.raise_for_status()
+
+
+def delete_experiment_run(id_, client):
+    response = requests.post("http://{}/v1/experiment-run/deleteExperimentRun".format(client._socket),
+                             json={'id': id_}, headers=client._auth)
+    response.raise_for_status()
