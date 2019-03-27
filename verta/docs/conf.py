b1a9cc32066371edcad4034651e99ab37a9d9457
Michael Liu <miliu@protonmail.com>
2019-03-27 13:42:13 -0700

    Add commented option to expand sidebar nests

--- verta/docs/conf.py
+++ verta/docs/conf.py
@@ -95,6 +95,7 @@ html_theme = 'alabaster'
 #
 html_theme_options = {
     'fixed_sidebar': True,
+    # 'sidebar_collapse': False,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
