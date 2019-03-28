a3520625f605634422861d74b8fdbb68e01dc9ca
Michael Liu <miliu@protonmail.com>
2019-03-28 15:31:34 -0700

    Fix name -> key bug

--- verta/verta/modeldbclient.py
+++ verta/verta/modeldbclient.py
@@ -1100,7 +1100,7 @@ class ExperimentRun:
         _utils.validate_flat_key(key)
 
         Message = _CommonService.GetAttributes
-        msg = Message(id=self._id, attribute_keys=[name])
+        msg = Message(id=self._id, attribute_keys=[key])
         data = _utils.proto_to_json(msg)
         response = requests.get("http://{}/v1/experiment-run/getAttributes".format(self._socket),
                                 params=data, headers=self._auth)
